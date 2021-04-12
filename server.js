b2188b42bd55e4f00d80e5f8cb63553826110b44
Fernando Molano <fm87usa@gmail.com>
2021-04-12 01:13:14 -0500

    DB setup for Heroku

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 let express = require("express");
 let api_routes = require("./routes/api");
+let path = require("path");
 
 // Create web app
 let app = express();
+
+let vueClientPath = path.join(__dirname, "student-sign-in-client", "dist");
+app.use(express.static(vueClientPath));
 
 // prepare app to be able to handle json data that's coming into the body of the request
 app.use(express.json());
